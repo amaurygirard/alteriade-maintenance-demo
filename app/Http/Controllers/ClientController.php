6b9322cc69fea63c83aac036fce39c8a78339733
Amaury Girard <girard@alteriade.fr>
2019-05-17 17:11:04 +0200

    Création d'un composant pour l'affichage de la liste des clients

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -16,7 +16,7 @@ class ClientController extends Controller
      */
     public function show($id)
     {
-        return view('client.profile', ['client' => Client::findOrFail($id)]);
+        return view('client.single', ['client' => Client::findOrFail($id)]);
     }
 
     /**
@@ -31,5 +31,36 @@ class ClientController extends Controller
         $clients = Client::all();
 
         return view('client.all', ['clients' => $clients]);
+    }
+
+    /**
+     * List all clients
+     *
+     * @param  int  $id
+     * @return View
+     */
+    public function listAll()
+    {
+
+        $clients = Client::all();
+
+        return view('welcome', ['clients' => $clients]);
+    }
+
+    /**
+     * Add new client
+     *
+     * @param  int  $id
+     * @return View
+     */
+    public function create(Request $request)
+    {
+      // Instanciation d'un nouveau client, auquel on attribue le nom passé en requête
+      $client = new Client;
+      $client->name = $request->name;
+
+      $client->save();
+
+      return redirect('/client/'.$client->id);
     }
 }
