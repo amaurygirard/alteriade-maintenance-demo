1b3801e017cbede31e4cd8b3bddb859086e30005
Amaury Girard <girard@alteriade.fr>
2019-05-17 10:06:25 +0200

    Import du html de l'intégration du projet au sein d'un template layout

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,303 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Document sans nom</title>
+
+		<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+
+		<!--jQuery-->
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js" type="text/javascript"></script>
+		<script src="js/main.js" type="text/javascript"></script>
+	</head>
+
+	<body>
+
+		<main>
+
+			<section id="main_section">
+
+				<div class="tag">LM</div>
+				<div class="tag">JR</div>
+
+				<h1><a href="#">Home</a> | <strong>Nom du projet</strong></h1>
+
+				<button id="projet_add"><span>Ajouter un nouveau projet</span></button>
+
+
+
+				<h2><strong>Contrats bientôt expirés</strong></h2>
+
+				<article class="bloc">
+
+					<div class="bloc_header">
+
+						<div class="bloc_header_container">
+
+							<h3><strong><a href="#">Lorem ipsum dolor.</a></strong></h3>
+
+							<div class="bloc_tags">
+								<div class="bloc_tag tag">LM</div>
+								<div class="bloc_tag tag">JR</div>
+							</div>
+
+						</div>
+
+					</div>
+
+					<div class="bloc_details bloc_marked bloc_warning">
+
+						<p class="bloc_details_main bloc_details_pictoed pictoed_calendar">
+
+							<span><strong>Lorem ipsum dolor.</strong></span>
+							<span>
+								<span class="txtright">Date de début du contrat</span>
+								<span class="bloc_details_countdown txtright">Temps restant</span>
+							</span>
+
+						</p>
+
+						<p class="bloc_details_below">
+
+							<span>Dernière intervention</span>
+							<span>le : Date</span>
+
+						</p>
+
+					</div>
+
+				</article>
+
+
+				<h2><strong>Tous les projets</strong></h2>
+
+				<article class="bloc bloc_closed">
+
+					<div class="bloc_header">
+
+						<div class="bloc_header_container">
+
+							<h3><strong><a href="#">Nom du projet</a></strong></h3>
+
+							<div class="bloc_tags">
+								<div class="bloc_tag tag">LM</div>
+								<div class="bloc_tag tag">JR</div>
+							</div>
+
+						</div>
+
+					</div>
+
+					<div class="bloc_details bloc_marked">
+
+						<p class="bloc_details_main bloc_details_pictoed pictoed_clock">
+
+							<span><strong>Lorem ipsum dolor.</strong></span>
+							<span>
+								<span class="txtright">Date de début du contrat</span>
+								<span class="bloc_details_countdown txtright">Date de fin de contrat</span>
+							</span>
+
+						</p>
+
+						<p class="bloc_details_below">
+
+							<span>Dernière intervention</span>
+							<span>le : Date</span>
+
+						</p>
+
+					</div>
+
+				</article>
+
+				<article class="bloc bloc_closed">
+
+					<div class="bloc_header">
+
+						<div class="bloc_header_container">
+
+							<h3><strong><a href="#">Lorem ipsum dolor.</a></strong></h3>
+
+							<div class="bloc_tags">
+								<div class="bloc_tag tag">LM</div>
+								<div class="bloc_tag tag">JR</div>
+							</div>
+
+						</div>
+
+					</div>
+
+					<div class="bloc_details bloc_marked">
+
+						<p class="bloc_details_main bloc_details_pictoed pictoed_clock">
+
+							<span><strong>Lorem ipsum dolor.</strong></span>
+							<span>
+								<span class="txtright">Date de début du contrat</span>
+								<span class="bloc_details_countdown txtright">Date de fin de contrat</span>
+							</span>
+
+						</p>
+
+						<p class="bloc_details_below">
+
+							<span>Dernière intervention</span>
+							<span>le : Date</span>
+
+						</p>
+
+					</div>
+
+				</article>
+
+				<article class="bloc bloc_closed">
+
+					<div class="bloc_header">
+
+						<div class="bloc_header_container">
+
+							<h3><strong><a href="#">Lorem ipsum dolor.</a></strong></h3>
+
+							<div class="bloc_tags">
+								<div class="bloc_tag tag">LM</div>
+								<div class="bloc_tag tag">JR</div>
+							</div>
+
+						</div>
+
+					</div>
+
+					<div class="bloc_details bloc_marked">
+
+						<p class="bloc_details_main bloc_details_pictoed pictoed_clock">
+
+							<span><strong>Lorem ipsum dolor.</strong></span>
+							<span>
+								<span class="txtright">Date de début du contrat</span>
+								<span class="bloc_details_countdown txtright">Date de fin de contrat</span>
+							</span>
+
+						</p>
+
+						<p class="bloc_details_below">
+
+							<span>Dernière intervention</span>
+							<span>le : Date</span>
+
+						</p>
+
+					</div>
+
+					<div class="bloc_details bloc_marked bloc_darkened">
+
+						<p class="bloc_details_main bloc_details_pictoed pictoed_clock">
+
+							<span><strong>Lorem ipsum dolor.</strong></span>
+							<span>
+								<span class="txtright">Date de début du contrat</span>
+								<span class="bloc_details_countdown txtright">Date de fin de contrat</span>
+							</span>
+
+						</p>
+					</div>
+
+				</article>
+
+
+				<h2><strong>Détail des contrats</strong></h2>
+
+				<article class="bloc bloc_closed">
+
+					<div class="bloc_header bloc_marked bloc_header_pictoed pictoed_clock">
+
+						<div class="bloc_header_container">
+
+							<h3><strong><a href="#">Nom du contrat</a></strong></h3>
+
+							<div class="bloc_tags">
+								<div class="bloc_tag tag">LM</div>
+								<div class="bloc_tag tag">JR</div>
+							</div>
+
+						</div>
+
+					</div>
+
+					<div class="bloc_details">
+
+						<p class="bloc_details_main bloc_details_pictoed pictoed_text pictoed_text_eab">
+
+							<span><strong>Nom de l'intervention</strong></span>
+							<span class="flex-container">
+								<span class="txtright">Intervention de : <strong>temps passé</strong></span>
+								<span class="bloc_details_countdown txtright">le : <strong>date</strong></span>
+							</span>
+
+						</p>
+
+						<p class="bloc_details_below">
+
+							<span>Description</span>
+
+						</p>
+
+					</div>
+
+					<div class="bloc_details bloc_marked bloc_darkened">
+
+						<p class="bloc_details_main bloc_details_pictoed pictoed_clock">
+
+							<span><strong>Lorem ipsum dolor.</strong></span>
+							<span>
+								<span class="txtright">Date de début du contrat</span>
+								<span class="bloc_details_countdown txtright">Date de fin de contrat</span>
+							</span>
+
+						</p>
+					</div>
+
+				</article>
+
+			</section>
+
+			<aside id="clients">
+
+				<div id="client_search">
+
+					<h2 class="h1-like"><strong>Clients</strong></h2>
+
+					<input type="text" name="client" id="client_input">
+
+				</div>
+
+				<ul id="client_list">
+
+					<li class="client_item"><a href="#">Lorem ipsum.</a></li>
+					<li class="client_item"><a href="#">Iste, nesciunt.</a></li>
+					<li class="client_item"><a href="#">Nisi, reprehenderit.</a></li>
+					<li class="client_item"><a href="#">Culpa, dolores.</a></li>
+					<li class="client_item"><a href="#">Voluptate, ea.</a></li>
+					<li class="client_item"><a href="#">Numquam, reprehenderit.</a></li>
+					<li class="client_item"><a href="#">Quam, provident.</a></li>
+					<li class="client_item"><a href="#">Itaque, ipsa.</a></li>
+					<li class="client_item"><a href="#">Officiis, fugit?</a></li>
+					<li class="client_item"><a href="#">Facilis, odit!</a></li>
+					<li class="client_item"><a href="#">Vel, quod?</a></li>
+					<li class="client_item"><a href="#">Eveniet, qui!</a></li>
+					<li class="client_item"><a href="#">Eos, quasi?</a></li>
+					<li class="client_item"><a href="#">Iusto, aut.</a></li>
+					<li class="client_item"><a href="#">Rem, possimus.</a></li>
+
+				</ul>
+
+				<button id="client_add">Ajouter un nouveau client</button>
+
+			</aside>
+
+		</main>
+
+		<footer><a href="#" title="alteriade">Copyright alteriade 2018</a></footer>
+
+	</body>
+</html>
