ff82827f7aab02e0b735a038d2f142a71c7d04ce
Amaury Girard <girard@alteriade.fr>
2019-06-04 10:05:46 +0200

    Nettoyage du layout

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -23,261 +23,37 @@
 		<section id="main_section">
 
 			{{-- Header de la section principale --}}
+
+			{{-- Tags des consultants et CECs --}}
 			@section('main_section_tags')
 			@show
 
+			{{-- Titre de la page & Fil d'Ariane --}}
 			<h1>
 				@section('main_section_breadcrumb')
 					<a href="/">Home</a>
 				@show
 			</h1>
 
+			{{-- Bouton 'ajouter' --}}
 			@section('main_section_add_button')
 			@show
 
+
 			{{--  Body de la section principale --}}
 			@section('main_section_body')
-				<h2><strong>Contrats bientôt expirés</strong></h2>
-
-				<article class="bloc">
-
-					<div class="bloc_header">
-
-						<div class="bloc_header_container">
-
-							<h3><strong><a href="#">Lorem ipsum dolor.</a></strong></h3>
-
-							<div class="bloc_tags">
-								<div class="bloc_tag tag">LM</div>
-								<div class="bloc_tag tag">JR</div>
-							</div>
-
-						</div>
-
-					</div>
-
-					<div class="bloc_details bloc_marked bloc_warning">
-
-						<p class="bloc_details_main bloc_details_pictoed pictoed_calendar">
-
-							<span><strong>Lorem ipsum dolor.</strong></span>
-							<span>
-								<span class="txtright">Date de début du contrat</span>
-								<span class="bloc_details_countdown txtright">Temps restant</span>
-							</span>
-
-						</p>
-
-						<p class="bloc_details_below">
-
-							<span>Dernière intervention</span>
-							<span>le : Date</span>
-
-						</p>
-
-					</div>
-
-				</article>
-
-
-				<h2><strong>Tous les projets</strong></h2>
-
-				<article class="bloc bloc_closed">
-
-					<div class="bloc_header">
-
-						<div class="bloc_header_container">
-
-							<h3><strong><a href="#">Nom du projet</a></strong></h3>
-
-							<div class="bloc_tags">
-								<div class="bloc_tag tag">LM</div>
-								<div class="bloc_tag tag">JR</div>
-							</div>
-
-						</div>
-
-					</div>
-
-					<div class="bloc_details bloc_marked">
-
-						<p class="bloc_details_main bloc_details_pictoed pictoed_clock">
-
-							<span><strong>Lorem ipsum dolor.</strong></span>
-							<span>
-								<span class="txtright">Date de début du contrat</span>
-								<span class="bloc_details_countdown txtright">Date de fin de contrat</span>
-							</span>
-
-						</p>
-
-						<p class="bloc_details_below">
-
-							<span>Dernière intervention</span>
-							<span>le : Date</span>
-
-						</p>
-
-					</div>
-
-				</article>
-
-				<article class="bloc bloc_closed">
-
-					<div class="bloc_header">
-
-						<div class="bloc_header_container">
-
-							<h3><strong><a href="#">Lorem ipsum dolor.</a></strong></h3>
-
-							<div class="bloc_tags">
-								<div class="bloc_tag tag">LM</div>
-								<div class="bloc_tag tag">JR</div>
-							</div>
-
-						</div>
-
-					</div>
-
-					<div class="bloc_details bloc_marked">
-
-						<p class="bloc_details_main bloc_details_pictoed pictoed_clock">
-
-							<span><strong>Lorem ipsum dolor.</strong></span>
-							<span>
-								<span class="txtright">Date de début du contrat</span>
-								<span class="bloc_details_countdown txtright">Date de fin de contrat</span>
-							</span>
-
-						</p>
-
-						<p class="bloc_details_below">
-
-							<span>Dernière intervention</span>
-							<span>le : Date</span>
-
-						</p>
-
-					</div>
-
-				</article>
-
-				<article class="bloc bloc_closed">
-
-					<div class="bloc_header">
-
-						<div class="bloc_header_container">
-
-							<h3><strong><a href="#">Lorem ipsum dolor.</a></strong></h3>
-
-							<div class="bloc_tags">
-								<div class="bloc_tag tag">LM</div>
-								<div class="bloc_tag tag">JR</div>
-							</div>
-
-						</div>
-
-					</div>
-
-					<div class="bloc_details bloc_marked">
-
-						<p class="bloc_details_main bloc_details_pictoed pictoed_clock">
-
-							<span><strong>Lorem ipsum dolor.</strong></span>
-							<span>
-								<span class="txtright">Date de début du contrat</span>
-								<span class="bloc_details_countdown txtright">Date de fin de contrat</span>
-							</span>
-
-						</p>
-
-						<p class="bloc_details_below">
-
-							<span>Dernière intervention</span>
-							<span>le : Date</span>
-
-						</p>
-
-					</div>
-
-					<div class="bloc_details bloc_marked bloc_darkened">
-
-						<p class="bloc_details_main bloc_details_pictoed pictoed_clock">
-
-							<span><strong>Lorem ipsum dolor.</strong></span>
-							<span>
-								<span class="txtright">Date de début du contrat</span>
-								<span class="bloc_details_countdown txtright">Date de fin de contrat</span>
-							</span>
-
-						</p>
-					</div>
-
-				</article>
-
-
-				<h2><strong>Détail des contrats</strong></h2>
-
-				<article class="bloc bloc_closed">
-
-					<div class="bloc_header bloc_marked bloc_header_pictoed pictoed_clock">
-
-						<div class="bloc_header_container">
-
-							<h3><strong><a href="#">Nom du contrat</a></strong></h3>
-
-							<div class="bloc_tags">
-								<div class="bloc_tag tag">LM</div>
-								<div class="bloc_tag tag">JR</div>
-							</div>
-
-						</div>
-
-					</div>
-
-					<div class="bloc_details">
-
-						<p class="bloc_details_main bloc_details_pictoed pictoed_text pictoed_text_eab">
-
-							<span><strong>Nom de l'intervention</strong></span>
-							<span class="flex-container">
-								<span class="txtright">Intervention de : <strong>temps passé</strong></span>
-								<span class="bloc_details_countdown txtright">le : <strong>date</strong></span>
-							</span>
-
-						</p>
-
-						<p class="bloc_details_below">
-
-							<span>Description</span>
-
-						</p>
-
-					</div>
-
-					<div class="bloc_details bloc_marked bloc_darkened">
-
-						<p class="bloc_details_main bloc_details_pictoed pictoed_clock">
-
-							<span><strong>Lorem ipsum dolor.</strong></span>
-							<span>
-								<span class="txtright">Date de début du contrat</span>
-								<span class="bloc_details_countdown txtright">Date de fin de contrat</span>
-							</span>
-
-						</p>
-					</div>
-
-				</article>
-
 			@show
+
 		</section>
 
+		{{-- Liste des clients : composant --}}
 		@component('components.client_list')
 		@endcomponent
 
 	</main>
 
+
+	{{-- Footer --}}
 	@php
 		$today = new \DateTime();
 	@endphp
