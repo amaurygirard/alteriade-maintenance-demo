669c9b0e7931c18e42ae56c1b447ebcedc9cd7e3
Amaury Girard <girard@alteriade.fr>
2020-07-16 10:03:14 +0200

    Correction de bug sur le composant 'modifier'

--- resources/views/components/modifier.blade.php
+++ resources/views/components/modifier.blade.php
@@ -1,2 +1,2 @@
 {{-- Bouton de modification --}}
-<a class="modifier reveal_on_hover" data-fancybox data-type="ajax" data-src="{{route('ajax_edit_'.$item->type,[$item->modelName.'_id' => $item->id])}}" href="javascript:;" title="Modifier le {{$item->modelName}}">[Modifier]</a>
+<a class="modifier reveal_on_hover" data-fancybox data-type="ajax" data-src="{{route('ajax_edit_'.$item->modelName,[$item->modelName.'_id' => $item->id])}}" href="javascript:;" title="Modifier le {{$item->modelName}}">[Modifier]</a>
